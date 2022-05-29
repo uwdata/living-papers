72f2ce83f5d82224038e01e3c88c1e878cb4a340
tu21897 <tunguyen2189721897@yahoo.com>
2022-05-28 19:52:08 -0700

    Unresolved citations, missing venues, missing abstracts, char limits

--- dev/cit-test/cite.md
+++ dev/cit-test/cite.md
@@ -11,3 +11,11 @@ Did you read @Scott:1985? It was part of the reading list [@Scott:1985; @Jones:1
 I liked @doi:10.1145/3242587.3242600, which should be the same as [@s2id:4fca64e6dc4e803d3ed904c04c6845a9e6adc53e] and [@s2id:52037222].
 
 We should also handle unresolved citations [@badcite].
+
+inproceedings @Bullman:2018
+
+article @Deriche:1990
+
+misc @Deriche:1993
+
+toomanyauthors @Toomanyauthors:2018
--- src/components/cite-ref.js
+++ src/components/cite-ref.js
@@ -22,67 +22,138 @@ export class CiteRef extends ArticleElement {
     this.__suffix = nodes[1];
   }
 
+  // TODO: 1. expand content on info/authors
+  //       2. citation interactive features
+  //       3. citation out of viewport behavior
   render() {
     const { key, data, index, mode } = this;
-    if (data == null) { return html`<span class ='citation-err'>!!<span class ='cit-err'>Unresolved Citation</span></span>`; }
-    const title = tooltip(data, key, index);
-    const body = (mode === 'inline-author' ? inline(data, index) : index) || '??';
-    // <!-- <div class='cit-info-item'>${data.venue}</div> -->
+
+    // Missing data
+    if (data == null) { return unresolvedCitation() }
+
+    // Missing minimum of title, year, and author data
+    if (data.title == null || data.year == null || data.author == null) { return unresolvedCitation() }
+
+    // Citation contents
+    const title = data.title;
+    const year = itemInfo('' + data.year, 30);
+    const venue = itemInfo('' + data.venue, 30); // Blank if missing
+    const authors = authorsBody(data);
+    const abstract = abstractBody(data, 300);
+
+    // Inline content
+    const body = mode === 'inline-author' ? inlineContent(data) : index;
+
     return html`
     <span class='citation'>${body}<span class='cit'>
         <div class='cit-head'>
-          <div class='cit-head-title'>${data.title}</div>
-          <div class='cit-head-info'>
-            <div class='cit-info-item'>${data.year}</div>
-          </div>
+          <div class='cit-head-title'>${title}</div>
+          <div class='cit-head-info'>${year}${venue}</div>
         </div>
-        <div class='cit-body'>
-          <div class='cit-body-auth'>${authors(data)}</div>
-          <div class='cit-body-abst'>${limitAbstract(data.abstract, 300)}</div>
-        </div>
-      </span>
-    </span> 
-    `;
+        <div class='cit-body'>${authors}${abstract}</div>
+      </span></span>`;
   }
 }
 
-function limitAbstract(abstract, charLimit) {
-  if (abstract == null) { return 'No abstract is available for this article.' }
-  return abstract.length > charLimit ? abstract.substring(0, charLimit) + '...' : abstract;
-}
-
-function tooltip(data, key, index) {
-  return data
-    ? `${authors(data)} (${data.year}). ${data.title}.${data.venue ? ` ${data.venue}.` : '' }`
-    : !index ? `Unresolved citation: ${key}`
-    : null;
-}
-
-function authors(data, etal = 2) {
+// Inline content
+function inlineContent(data) {
   const { author } = data;
-  if (author.length > etal) {
-    const { given, family } = author[0];
-    return `${given[0]}. ${family} et al.`;
-  } else {
-    return author
-      .map(({ given, family }) => `${given[0]}. ${family}`)
-      .join(', ');
+
+  let authors = author[0].family;
+  if (author.length === 2) {
+    authors += ` & ${author[1].family}`;
+  } else if (author.length > 2) {
+    authors += ' et al.';
   }
+
+  return authors;
 }
 
-function inline(data, index) {
-  let authors = '';
+// Returns an unresolved citation
+function unresolvedCitation() {
+  return html`<span class ='citation-err'>??<span class ='cit-err'>Unresolved Citation</span></span>`;
+}
 
-  if (data && data.author) {
-    const { author } = data;
-    authors = author[0].family;
-    if (author.length === 2) {
-      authors += ` & ${author[1].family}`;
-    } else if (author.length > 2) {
-      authors += ' et al.';
+// Returns a div containing info items
+function itemInfo(info, charLimit) {
+  const infoText = limitTokens(info, charLimit);
+
+  // Return the corresponding styled div class
+  return html`<div class='cit-info-item'>${infoText}</div>`;
+}
+
+// Returns a div containing the authors displayed in the body of the citation
+function authorsBody(data, etal = 10) {
+  const { author } = data;
+  // List all authors with initials for first/middle name followed by last name
+  const authors = author.map(({ given, family }) => `${given.includes('.') ? given:given[0] + '.'} ${family}`);
+
+  // Create text string, if there are more than authors 10 authors, abbreviate with et al
+  const authText = authors.length > etal ? authors.slice(0,10).join(', ') + ' et al.': authors.join(', ');
+
+  // Return the corresponding styled div class
+  return html`<div class='cit-body-auth'> ${authText}</div>`;
+}
+
+// Returns a div containing the abstract displayed in the body of the citation
+function abstractBody(data, charLimit) {
+  const { abstract } = data;
+  // Default no abstract
+  let absText = 'No abstract is available for this article.';
+
+  // Abstract is in data
+  if (abstract != null) { absText = limitTokens(abstract, charLimit); }
+
+  // Return the corresponding styled div class
+  return html`<div class='cit-body-abst'>${absText}</div>`;
+}
+
+// Returns a string with less characters than the character limit
+function limitTokens(input, charLimit) {
+  let tokens = input.split(' ');
+  let text = '';
+  for (let i = 0; i < tokens.length; i++) {
+    if ((text + tokens[i]).length > charLimit) { // If it is longer than the char limit, stop and add ellipses
+      text += '... ';
+      break;
     }
-    authors = html`${authors}&nbsp;`;
+    text += ' ' + tokens[i];
   }
-
-  return html`${authors}<span class="cite-list">${index}</span>`;
+  return text;
 }
+
+// function tooltip(data, key, index) {
+//   return data
+//     ? `${authors(data)} (${data.year}). ${data.title}.${data.venue ? ` ${data.venue}.` : '' }`
+//     : !index ? `Unresolved citation: ${key}`
+//     : null;
+// }
+
+// function authors(data, etal = 2) {
+//   const { author } = data;
+//   if (author.length > etal) {
+//     const { given, family } = author[0];
+//     return `${given[0]}. ${family} et al.`;
+//   } else {
+//     return author
+//       .map(({ given, family }) => `${given[0]}. ${family}`)
+//       .join(', ');
+//   }
+// }
+
+// function inline(data, index) {
+//   let authors = '';
+
+//   if (data && data.author) {
+//     const { author } = data;
+//     authors = author[0].family;
+//     if (author.length === 2) {
+//       authors += ` & ${author[1].family}`;
+//     } else if (author.length > 2) {
+//       authors += ' et al.';
+//     }
+//     authors = html`${authors}&nbsp;`;
+//   }
+
+//   return html`${authors}<span class="cite-list">${index}</span>`;
+// }
