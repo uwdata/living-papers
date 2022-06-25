ee0121cc0bb1d1f1c1b9183f4e6e4fc054490e6a
jheer <jeffrey.heer@gmail.com>
2022-06-25 23:29:04 +0200

    feat: Remove unused cite-bib component.

--- src/components/cite-bib.js
+++
@@ -1,8 +0,0 @@
-import { html } from 'lit';
-import { ArticleElement } from './article-element.js';
-
-export class CiteBib extends ArticleElement {
-  render() {
-    return html`<h1>References</h1>${this.__children}`;
-  }
-}
