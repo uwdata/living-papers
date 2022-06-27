7c5f395fda44d56434890695981491695a428301
jheer <jeffrey.heer@gmail.com>
2022-06-27 14:21:06 +0200

    feat: Update cross-reference navigation.

--- src/components/cross-ref.js
+++ src/components/cross-ref.js
@@ -11,10 +11,29 @@ export class CrossRef extends ArticleElement {
     };
   }
 
+  goto(event) {
+    // prevent jump to referenced element
+    event.preventDefault();
+
+    // add id to hash component of current URL
+    // do not change the page undo history
+    const id =`#${this.xref}`;
+    history.replaceState(null, null, id);
+
+    // smoothly scroll to referenced component
+    document.querySelector(id)
+      .scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+  }
+
   render() {
     // TODO? title tooltip
-    const { type, xref, index = '?', short } = this;
-    const cls = `cross-ref ${type}${!short ? ' full' : ''}${index === '?' ? ' unresolved' : ''}`;
-    return html`<a class=${cls} href=${`#${xref}`}>${index}</a>`;
+    const { type, xref, index, short } = this;
+    const resolved = index != null;
+    const cls = `cross-ref ${type}${!short ? ' full' : ''}`;
+    if (resolved) {
+      return html`<a class=${cls} href="#${xref}" @click=${this.goto}>${index}</a>`;
+    } else {
+      return html`<a class="${cls} unresolved" title="Unresolved reference: ${xref}">?</a>`;
+    }
   }
 }
