88dcc79107fe3d24e9f31733d49b063a18fbdd41
Tu Nguyen <73036803+tu21897@users.noreply.github.com>
2022-07-13 04:25:53 -0700

    Citation revisions and accessibility features (#16)
    * Revised event handling and general style changes
    * Fixed citations opening on every key, citations close out of focus
    * Unicode for arrows

--- src/components/cite-ref.js
+++ src/components/cite-ref.js
@@ -15,33 +15,43 @@ export class CiteRef extends ArticleElement {
   constructor() {
     super();
     this.mode = 'citation';
+    this.addEventListener('keydown', this.keyDown);
+    this.addEventListener('mousedown', this.openCitation);
+    this.addEventListener('focusout', this.focusOut);
+  }
 
-    const hoverDelay = 300;
-    let active = null;
+  focusOut(event) {
+    if (!this.contains(event.relatedTarget)) {
+      this.closeCitation();
+    }
+  }
 
-    // On hover, open the citation after a certain amount of time
-    this.addEventListener('mouseenter', () => {
-      if (active !== null) {
-        clearTimeout(active);
-        active = null;
-      }
-      active = setTimeout(
-        () => this.querySelector('.cite-info').style.display = 'inline',
-        hoverDelay
-      );
-    });
+  keyDown(event) {
+    if (event.key == 'Enter') {
+      this.openCitation();
+    } else if (event.key != 'Tab') {
+      this.closeCitation();
+    }
+  }
 
-    // On hover off, close the citation after a certain amount of time
-    this.addEventListener('mouseleave', () => {
-      if (active !== null) {
-        clearTimeout(active);
-        active = null;
-      }
-      active =  setTimeout(
-        () => this.querySelector('.cite-info').style.display = 'none',
-        hoverDelay
-      );
-    });
+  closeCitation() {
+    for (const child of this.querySelector('.cite-ref').children) {
+      child.style.display = 'none';
+    }
+  }
+
+  openCitation() {
+    for (const child of this.querySelector('.cite-ref').children) {
+      child.style.display = 'inline-block';
+    }
+  }
+
+  // Expand and collapse info content
+  toggleContent(event) {
+    const summary = event.target.firstChild;
+    event.target.hasAttribute('open') 
+      ? summary.textContent = summary.getAttribute('data-longtext') 
+      : summary.textContent = summary.getAttribute('data-shorttext');
   }
 
   initialChildNodes(nodes) {
@@ -51,26 +61,36 @@ export class CiteRef extends ArticleElement {
 
   render() {
     const { key, data, index, mode} = this;
+    const wrapper = html`<div class='cite-info-wrapper'></div>`;
 
     // Unresolved citation
     if (data == null) {
-      return html`<span class='cite-ref unresolved'>??<div class='cite-info'>
-<div class='cite-info-arrow'></div><b>Unresolved citation</b><br>"${key}"</div></span>`;
+      return html`<span class='cite-ref unresolved'>??${wrapper}<div class='cite-info'>
+        <b>Unresolved citation</b><br>"${key}"</div></span>`;
     }
 
+    const { fullInfo, shortInfo } = infoBody(data);
+    const desctext = descBody(data);
+
     // Citation contents
-    const arrow = html`<div class='cite-head-arrow'></div>`;
-    const title = html`<div class='cite-head-title'>${data.title}</div>`;
     const subtitle = data.venue ? html`<div class='cite-head-subtitle'>${data.venue}</div>` : null;
-    const info = infoBody(data);
-    const desc = descBody(data);
+    const info = shortInfo 
+      ? html`<details class='cite-body-auth' @toggle=${this.toggleContent}>
+              <summary data-shorttext=${shortInfo} data-longtext=${fullInfo}>${shortInfo}</summary>
+            </details>`
+      : html`<div class='cite-body-auth'>${fullInfo}</div>`;
+    const desc = data.abstract && data.abstract !== desctext 
+      ? html`<details class='cite-body-desc' @toggle=${this.toggleContent}>
+              <summary data-shorttext=${desctext} data-longtext=${data.abstract}>${desctext}</summary>
+            </details>`
+      : html`<div class='cite-body-desc'>${desctext}</div>`;
 
     // Inline content
     const body = mode === 'inline-author' ? inlineContent(data, index) : index;
 
-    return html`<span class='cite-ref'>${body}<div class='cite-info'>
-      <a class='cite-head' href=${data.url} target="_blank" rel="noopener noreferrer" style="color: inherit; text-decoration: none;">
-        ${arrow}${title}${subtitle}
+    return html`<span class='cite-ref' tabindex=0>${body}${wrapper}<div class='cite-info'>
+      <a class='cite-head' href=${data.url} target="_blank" rel="noopener noreferrer" style="color: inherit;">
+        <div class='cite-head-title'>${data.title}</div>${subtitle}
       </a>
       <div class='cite-body'>
         ${info}${desc}
@@ -90,60 +110,27 @@ function inlineContent(data, index, etal=2) {
     authors += ' et al.';
   }
 
-  return authors + ' [' + index + ']';
+  return `${authors} [${index}]`;
 }
 
-// Returns the info portion of the body, with authors and year,
-// if more than max authors add the option to expand/collapse section
-function infoBody(data, max=2) {
+function infoBody(data, maxAuthors=2) {
   const { author, year } =  data;
 
-  const aMap = author.map(({ given, family }) => `${given.includes('.') ? given:given[0] + '.'} ${family}`);   // List all authors as F.M. Last
-  const info = '' + year + ' \u2022 ' + aMap.join(', '); // Full info section
-  let shortInfo = info; // Shortened info section
-  let button = null; // Expand/collapse button, null if unneeded
+  const aMap = author.map(({ given, family }) => `${given.includes('.') ? given:given[0] + '.'} ${family}`);
+  const fullInfo = `${year} \u2022 ${aMap.join(', ')}`;
+  const shortInfo = aMap.length > maxAuthors 
+    ? `${year} \u2022 ${aMap.slice(0, maxAuthors).join(', ')} +${aMap.length - maxAuthors}` 
+    : null;
 
-  if (aMap.length > max) {
-    const authNum = '+' + (aMap.length - max); // Representation of authors past max
-    shortInfo = '' + year + ' \u2022 ' + aMap.slice(0, max).join(', '); // Shortened info section
-
-    // Button event, update button info, rotate arrow, swap to the corresponding text
-    const onClick = "let x=event.target.parentElement.querySelector('span')||event.target;let y=x.parentElement;let z=y.parentElement;"+
-                    "(x.style.transform==='rotate(0deg)')"+
-                    "?(x.style.transform='rotate(-90deg)',z.childNodes[1].nodeValue=y.getAttribute('shortinfo'),y.childNodes[1].textContent=y.getAttribute('authnum'))"+
-                    ":(x.style.transform='rotate(0deg)',z.childNodes[1].nodeValue=y.getAttribute('info'),y.childNodes[1].textContent='\u00A0')";
-    const style = "display: inline; border: none; background: none;";
-    const arrow = html`<span style='position: absolute; transform: rotate(-90deg);'>&#9660;</span>`;
-
-    button = html`<button style=${style} onclick=${onClick} info=${info} shortinfo=${shortInfo} authnum=${authNum}>${authNum}${arrow}</button>`;
-  }
-
-  return html`<div class='cite-body-auth'>${shortInfo}${button}</div>`;
+  return {fullInfo, shortInfo};
 }
 
-// Returns the description portion of the body, limits the description by tokens if over char limit
 function descBody(data, charLimit=300) {
   const { abstract, tldr } = data;
 
-  // Prioritize tldr, then abstract, if none use error message.
-  let shortDesc = tldr || abstract || 'No description is available for this article.';
-
-  // Limit the token characters to under the character limit
-  if (shortDesc.length > charLimit) {
-     shortDesc = shortDesc.substring(0, shortDesc.substring(0, charLimit).lastIndexOf(' ')) + '... ';
-  }
-
-  let button = null; // Expand/collapse button, null if unneeded
-
-  if (abstract != null) {
-    // Button event, rotate arrow, swap to the corresponding text
-    const onClick = "(event.target.style.transform==='rotate(0deg)')"+
-                    "?(event.target.style.transform='rotate(-90deg)',event.target.parentElement.childNodes[1].textContent=event.target.getAttribute('shortdesc'))"+
-                    ":(event.target.style.transform='rotate(0deg)',event.target.parentElement.childNodes[1].textContent=event.target.getAttribute('abstract'))";
-    const style = "position: absolute; left: calc(50% - 16px); bottom: 0; border: none; background: none; transform: rotate(-90deg); font-size: 16px;";
-
-    button = html`<button style=${style} onclick=${onClick} abstract=${abstract} shortdesc=${shortDesc}>&#9660;</button>`;
-  }
-
-  return html`<div class='cite-body-desc'>${shortDesc}${button}</div>`;
+  const shortDesc = tldr || abstract || 'No description is available for this article.';
+  
+  return shortDesc.length > charLimit 
+    ? shortDesc.substring(0, shortDesc.substring(0, charLimit).lastIndexOf(' ')) + '... ' 
+    : shortDesc;
 }
