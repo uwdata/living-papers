8ddbbf9313f265ade7708e700538a0e3d9d8c6db
Joshua Horowitz <joshuah@alum.mit.edu>
2022-08-24 18:09:32 -0700

    fix: avoid extractText crashes from DoubleQuote and such

--- src/parse/parse-util.js
+++ src/parse/parse-util.js
@@ -14,6 +14,7 @@ export function append(array, items) {
 }
 
 export function extractText(blocks) {
+  if (!blocks) { return ''; }
   return blocks.map(block => {
     const { t: type, c: content } = block;
     switch (type) {
