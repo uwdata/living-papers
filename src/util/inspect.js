e47f6d5f15ff0efa663cfc1c864107d503967145
Joshua Horowitz <joshuah@alum.mit.edu>
2022-08-31 16:47:02 -0700

    `streamToString` for debugging

--- src/util/inspect.js
+++ src/util/inspect.js
@@ -1,5 +1,16 @@
 import util from 'node:util';
+import { Buffer } from 'node:buffer';
 
 export function inspect(value) {
   return util.inspect(value, {showHidden: false, depth: null, colors: true});
+}
+
+// from https://stackoverflow.com/questions/10623798/how-do-i-read-the-contents-of-a-node-js-stream-into-a-string-variable
+export function streamToString(stream) {
+  const chunks = [];
+  return new Promise((resolve, reject) => {
+    stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
+    stream.on('error', (err) => reject(err));
+    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
+  })
 }
