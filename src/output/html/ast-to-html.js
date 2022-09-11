bf7a7f6b33f70fd98e0dd5198aeb8c0286975c88
Joshua Horowitz <joshuah@alum.mit.edu>
2022-09-10 22:07:02 -0700

    add support for 'raw' to html output

--- src/output/html/ast-to-html.js
+++ src/output/html/ast-to-html.js
@@ -1,6 +1,7 @@
 import { aliasComponent, aliasProperty } from './alias.js';
 import { htmlEscape } from '../../util/html-escape.js';
 import { DATA_ATTR } from './constants.js';
+import { getPropertyValue } from '../../ast/index.js';
 
 export function astMountHTML(ast) {
   const { html, attrs, events } = astToHTML(ast);
@@ -24,6 +25,15 @@ function renderNode(node, ctx) {
 
   if (type === 'textnode') {
     return htmlEscape(node.value);
+  }
+
+  if (node.name === 'raw') {
+    const format = getPropertyValue(node, 'format');
+    if (format !== 'html') {
+      return '';
+    } else {
+      return node.children[0].value;
+    }
   }
 
   const name = aliasComponent(node.name);
