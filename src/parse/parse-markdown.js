f8952805f25abc4b28726248ee5eb088084d85f2
Joshua Horowitz <joshuah@alum.mit.edu>
2022-09-12 22:18:20 -0700

    oh yeah let's not output all that stuff

--- src/parse/parse-markdown.js
+++ src/parse/parse-markdown.js
@@ -1,6 +1,8 @@
 import { pandoc } from './pandoc.js';
 import { parsePandocAST } from './parse-pandoc-ast.js';
 import { preprocess } from './preprocess.js';
+import { writeFile } from '../util/fs.js';
+import { streamToString } from '../util/inspect.js';
 
 export async function parseMarkdown(options) {
   const {
@@ -14,6 +16,16 @@ export async function parseMarkdown(options) {
     sourceExtensions: ['-implicit_figures'],
     stdin: await preprocess(inputFile)
   });
+
+  // await writeFile(
+  //   'preprocessed.md',
+  //   await streamToString(await preprocess(inputFile))
+  // )
+
+  // await writeFile(
+  //   'pandoc.json',
+  //   JSON.stringify(doc, null, 2)
+  // );
 
   const { metadata, article } = parsePandocAST(doc, parseContext);
 
