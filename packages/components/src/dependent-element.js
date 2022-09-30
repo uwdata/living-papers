8a06a3b5c722a5497f5ded28ec930b50f251af9f
jheer <jeffrey.heer@gmail.com>
2022-09-30 15:38:39 -0700

    docs: Add components package documentation.

--- packages/components/src/dependent-element.js
+++ packages/components/src/dependent-element.js
@@ -1,8 +1,15 @@
 import { ArticleElement } from './article-element.js';
 import { getDependency, hasDependencies, loadDependencies } from './util/dependencies.js';
 
+/**
+ * Abstract base class for Living Papers custom elements
+ * with dependencies loaded from a CDN upon page load.
+ * Implementations must define a static `dependencies` getter
+ * that returns a list of dependency definitions.
+ */
 export class DependentElement extends ArticleElement {
   getDependency(name) {
+    // proxy calls to the getDependency helper
     return getDependency(this, name);
   }
 
