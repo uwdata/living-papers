c880debdcedd99ee20af702e0061b2dc666b9dd3
jheer <jeffrey.heer@gmail.com>
2022-11-05 12:26:19 -0700

    fix: Remove unneeded fetch in input options.

--- packages/compiler/src/config.js
+++ packages/compiler/src/config.js
@@ -13,7 +13,6 @@ export async function inputOptions(context) {
   return {
     inputFile,
     inputType: inputType || inferInputType(inputFile, logger),
-    fetch,
     logger,
     ...parse
   }
