0160b3f6bf9f0640871d654280f2d9e2674775db
jheer <jeffrey.heer@gmail.com>
2023-03-03 12:04:00 -0800

    feat: Add appendix section support.

--- packages/compiler/src/plugins/section/index.js
+++ packages/compiler/src/plugins/section/index.js
@@ -1,27 +1,42 @@
 import {
-  createComponentNode, createProperties, createTextNode, replaceChild, visitNodes
+  createComponentNode, createProperties, createTextNode, replaceChild,
+  setValueProperty, visitNodes
 } from '@living-papers/ast';
 
 // TODO pass in from context?
 const aliases = new Map([
   ['abstract', 'Abstract'],
   ['acknowledgments', 'Acknowledgments'],
+  ['appendix', null],
   ['references', 'References']
 ]);
+
+function isHeader(node) {
+  return node.name && /h\d/.test(node.name);
+}
 
 // TODO maintain one-to-one top-level AST mapping (div.name)?
 export default function(ast) {
   visitNodes(ast.article, (node, parent) => {
     if (aliases.has(node.name)) {
+      // special sections should not contain numbered sections
+      visitNodes(node, child => {
+        if (isHeader(child)) {
+          setValueProperty(child, 'nonumber', true);
+        }
+      });
+
+      // extract section content
+      // add a header if alias is defined
       const alias = aliases.get(node.name);
-      replaceChild(parent, node, [
+      const header = alias ? [
         createComponentNode(
           'h1',
           createProperties({ nonumber: true }),
           [ createTextNode(alias) ]
-        ),
-        ...node.children
-      ]);
+        )
+      ] : [];
+      replaceChild(parent, node, [...header, ...node.children]);
     }
   });
   return ast;
