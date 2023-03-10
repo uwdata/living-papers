72e38ee0dc2472d94b8570e3a73644cdae05fc63
jheer <jeffrey.heer@gmail.com>
2023-03-09 18:59:08 -0800

    fix: Do not transform include raw format value.

--- packages/compiler/src/plugins/include/index.js
+++ packages/compiler/src/plugins/include/index.js
@@ -47,13 +47,12 @@ export default async function(ast, context) {
 }
 
 async function getIncludedContent(node, context) {
-  const raw = getPropertyValue(node, 'raw');
+  const format = getPropertyValue(node, 'raw');
   const file = getPropertyValue(node, 'file');
   const inputFile = path.join(context.inputDir, file);
   try {
-    if (raw) {
+    if (format) {
       // include as raw format-specific content
-      const format = raw === 'latex' ? 'tex' : raw;
       return [
         createComponentNode(
           'raw',
