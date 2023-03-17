91d602f4f4be75b80d27ec566d9cb37c1719fd67
tu21897 <tunguyen2189721897@yahoo.com>
2023-03-16 20:29:04 -0700

    add resizable, moveable figure tooltips to cross-ref

--- packages/components/src/cross-ref.js
+++ packages/components/src/cross-ref.js
@@ -11,29 +11,83 @@ export class CrossRef extends ArticleElement {
     };
   }
 
-  goto(event) {
-    // prevent jump to referenced element
-    event.preventDefault();
+  constructor() {
+    super();
+    this.addEventListener('mousedown', this.show);
+  }
 
-    // add id to hash component of current URL
-    // do not change the page undo history
-    const id =`#${this.xref}`;
-    history.replaceState(null, null, id);
+  updated() {
+    this.dragElement(this.querySelector('.fig-controls'));
+  }
 
-    // smoothly scroll to referenced component
-    document.querySelector(id)
-      .scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+  hide() {
+    this.querySelector('.fig-tooltip').style.display = 'none';
+  }
+
+  show() {
+    this.querySelector('.fig-tooltip').style.display = 'inline-block';
+  }
+
+  minimizeFigure() {
+    const tooltip = this.querySelector('.fig-tooltip');
+    tooltip.style.width = '400px';
+    tooltip.style.height = 'auto';
+  }
+
+  dragElement(el) {
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    const tooltip = this.querySelector('.fig-tooltip');
+    el.onmousedown = dragMouseDown;
+    
+    function dragMouseDown(e) {
+      e = e || window.event;
+      e.preventDefault();
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = () => {
+        document.onmouseup = null;
+        document.onmousemove = null;
+      };
+      document.onmousemove = elementDrag;
+    }
+  
+    const elementDrag = (e) => {
+      e = e || window.event;
+      e.preventDefault();
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      tooltip.style.top = (tooltip.offsetTop - pos2) + "px";
+      tooltip.style.left = (tooltip.offsetLeft - pos1) + "px";
+    }
   }
 
   render() {
-    // TODO? title tooltip
     const { type, xref, index, short } = this;
     const resolved = index != null;
+    const id = `#${xref}`;
     const cls = `cross-ref ${type}${!short ? ' full' : ''}`;
     if (resolved) {
-      return html`<a class=${cls} href="#${xref}" @click=${this.goto}>${index}</a>`;
+      return html`
+        <span class=${cls}>${index}</span>
+        <div class="fig-tooltip">
+            <div class="fig-controls">
+                    <a class="fig-minimize" @click=${this.minimizeFigure}></a>
+                    <a class="fig-close" @click=${this.hide}/></a>
+            </div>
+            ${renderFigureTooltipBody(id)}
+        </div>`;
     } else {
       return html`<a class="${cls} unresolved" title="Unresolved reference: ${xref}">?</a>`;
     }
   }
+}
+
+function renderFigureTooltipBody(id) {
+  const figCopy = document.querySelector(id).cloneNode({deep:true});
+  figCopy.className = 'figure';
+  figCopy.removeAttribute('id');
+  figCopy.querySelector('figcaption').removeAttribute('class');
+  return html`<div class="fig-body">${figCopy}</div>`;
 }
