75dbfc3cdfa3ae5f71375cfc58287c4c8e8afff2
tu21897 <tunguyen2189721897@yahoo.com>
2023-03-20 18:12:40 -0700

    render equations, reactive runtime, figures in cross-ref tooltip

--- packages/components/src/cross-ref.js
+++ packages/components/src/cross-ref.js
@@ -1,7 +1,7 @@
 import { html } from 'lit';
-import { ArticleElement } from './article-element';
+import { Tooltip } from './tooltip';
 
-export class CrossRef extends ArticleElement {
+export class CrossRef extends Tooltip {
   static get properties() {
     return {
       type: {type: String},
@@ -11,83 +11,45 @@ export class CrossRef extends ArticleElement {
     };
   }
 
-  constructor() {
-    super();
-    this.addEventListener('mousedown', this.show);
-  }
-
-  updated() {
-    this.dragElement(this.querySelector('.fig-controls'));
-  }
-
-  hide() {
-    this.querySelector('.fig-tooltip').style.display = 'none';
+  renderRefContent(ref) {
+    const tooltip = this.querySelector('.cross-ref-tooltip');
+    tooltip.innerHTML = '';
+    cloneReference(tooltip, ref, ['id', 'code'], ['cite-ref', 'eqn-num']);
+    if (this.type === 'fig') this.querySelector('.figure').className = 'figure';
   }
 
   show() {
-    this.querySelector('.fig-tooltip').style.display = 'inline-block';
-  }
-
-  minimizeFigure() {
-    const tooltip = this.querySelector('.fig-tooltip');
-    tooltip.style.width = '400px';
-    tooltip.style.height = 'auto';
-  }
-
-  dragElement(el) {
-    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    const tooltip = this.querySelector('.fig-tooltip');
-    el.onmousedown = dragMouseDown;
-    
-    function dragMouseDown(e) {
-      e = e || window.event;
-      e.preventDefault();
-      pos3 = e.clientX;
-      pos4 = e.clientY;
-      document.onmouseup = () => {
-        document.onmouseup = null;
-        document.onmousemove = null;
-      };
-      document.onmousemove = elementDrag;
-    }
-  
-    const elementDrag = (e) => {
-      e = e || window.event;
-      e.preventDefault();
-      pos1 = pos3 - e.clientX;
-      pos2 = pos4 - e.clientY;
-      pos3 = e.clientX;
-      pos4 = e.clientY;
-      tooltip.style.top = (tooltip.offsetTop - pos2) + "px";
-      tooltip.style.left = (tooltip.offsetLeft - pos1) + "px";
-    }
+    if (this.index != null) this.renderRefContent(document.getElementById(this.xref));
+    this.querySelector('.tooltip').style.display = 'inline-block';
   }
 
   render() {
     const { type, xref, index, short } = this;
     const resolved = index != null;
-    const id = `#${xref}`;
-    const cls = `cross-ref ${type}${!short ? ' full' : ''}`;
-    if (resolved) {
-      return html`
-        <span class=${cls}>${index}</span>
-        <div class="fig-tooltip">
-            <div class="fig-controls">
-                    <a class="fig-minimize" @click=${this.minimizeFigure}></a>
-                    <a class="fig-close" @click=${this.hide}/></a>
-            </div>
-            ${renderFigureTooltipBody(id)}
-        </div>`;
-    } else {
-      return html`<a class="${cls} unresolved" title="Unresolved reference: ${xref}">?</a>`;
-    }
+    const cls = `cross-ref ${type}${!short ? ' full' : ''}${resolved ? '' : ' unresolved'}`;
+    const content = index || '??';
+    return this.renderWithTooltip(cls, content, renderTooltipContent(resolved, xref));
   }
 }
 
-function renderFigureTooltipBody(id) {
-  const figCopy = document.querySelector(id).cloneNode({deep:true});
-  figCopy.className = 'figure';
-  figCopy.removeAttribute('id');
-  figCopy.querySelector('figcaption').removeAttribute('class');
-  return html`<div class="fig-body">${figCopy}</div>`;
+function renderTooltipContent(resolved, xref) {
+  if (resolved) {
+    return html`<div class="cross-ref-tooltip"></div>`;
+  } else {
+    return html`<div class="cross-ref-error-tooltip">Unresolved reference: ${xref}</div>`;
+  }
+}
+
+function cloneReference(parent, node, skipAttrs=[], skipClasses=[]) {
+  for (const cls of skipClasses) {
+    if (`${node.className}`.includes(cls)) return;
+  }
+  const clone = node.cloneNode();
+  for (const attr of skipAttrs) {
+    if (clone[attr]) clone.removeAttribute(attr);
+  }
+  parent.append(clone);
+  for (const child of node.childNodes) {
+    cloneReference(clone, child, skipAttrs, skipClasses);
+  }
 }
