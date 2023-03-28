4612ea3870e01ccd949429891c1786c21651217c
tu21897 <tunguyen2189721897@yahoo.com>
2023-03-28 14:39:41 -0700

    calculate tooltip x translation, use borderbox for y translation

--- packages/components/src/tooltip.js
+++ packages/components/src/tooltip.js
@@ -34,9 +34,9 @@ export class Tooltip extends ArticleElement {
   show() {
     const bbox = this.getBoundingClientRect();
     const ttip = this.querySelector('.tooltip');
-    ttip.style.transform = `translate(${bbox.left - bbox.width < 0 ? 0 : -bbox.width}px, calc(1em + 6px))`;
     ttip.style.display = 'inline-block';
     this.visible = true;
+    transformTooltip(bbox, ttip);
 
     // check if the user has clicked outside of the parent element
     const mouseDownClose = (event) => {
@@ -58,4 +58,25 @@ export class Tooltip extends ArticleElement {
     const tip = html`<div class="tooltip">${tooltip}</div>`;
     return html`<span class=${classes} tabindex=0>${body}${tip}</span>`;
   }
+}
+
+function transformTooltip(bbox, ttip) {
+  ttip.style.transform = `translate(0, 0)`;
+  const ttbb = ttip.getBoundingClientRect();
+  const hasMultiLineBody =  bbox.width > ttbb.left;
+  const docWidthWithRightPad = document.body.clientWidth - 16;
+  const translateY = (hasMultiLineBody ? bbox.height/2 : bbox.height) + 4;
+
+  // case 1: tooltip is wider than document, shift to the leftmost point
+  // case 2: tooltip extends out of view, shift it back that amount
+  // case 3: tooltip wraps lines, shift it to leftmost point of tooltip body
+  // otherwise, shift it back the width of tooltip body
+  const translateX = ttbb.width > docWidthWithRightPad
+    ? -ttbb.left
+    : ttbb.right > docWidthWithRightPad
+    ? docWidthWithRightPad - ttbb.right
+    : hasMultiLineBody
+    ? bbox.left - ttbb.left : -bbox.width;
+
+  ttip.style.transform = `translate(${translateX}px, ${translateY}px)`;
 }
